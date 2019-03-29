ba0693c8103f713559d09dc83ac6cad3d9aac02f
Mike Timberg <mike.timberg@technicity.io>
2019-03-29 12:19:45 -0400

    Tests written for incorrect commands.

--- test/data.js
+++ test/data.js
@@ -1,4 +1,8 @@
 const expected = [
+  `Alex: 42 miles @ 34 mph\n` +
+  `Dan: 39 miles @ 47 mph\n` +
+  `Bob: 0 miles\n`,
+  //
   `Alex: 335 miles @ 56 mph\n` +
   `Dan: 264 miles @ 58 mph\n`,
   //
@@ -13,6 +17,7 @@ const expected = [
   `Error: Bad time format for \`"Trip"\` command.\n` +
   `Error: Bad miles format for \`"Trip"\` command.\n` +
   `Error: Driver \`"Alex"\` not registered.\n` +
+  `Error: Command \`"Remove"\` not recognized.\n` +
   `Bob: 0 miles\n\n`
 ]
 
--- test/test.js
+++ test/test.js
@@ -2,8 +2,10 @@ const data = require(`./data`)
 const { test } = require(`./utils/index`)
 
 test.runner([
-  { command: `node server.js ./test/inputPass.txt`, expected: { data: data.expected[0], pass: true } },
-  { command: `cat ./test/inputPass.txt | node server.js`, expected: { data: data.expected[0], pass: true } },
-  { command: `node server.js ./test/inputFail.txt`, expected: { data: data.expected[1], pass: false } },
-  { command: `cat ./test/inputFail.txt | node server.js`, expected: { data: data.expected[1], pass: false } }
+  { command: `node server.js ./test/input.txt`, expected: { data: data.expected[0], pass: true } },
+  { command: `cat ./test/input.txt | node server.js`, expected: { data: data.expected[0], pass: true } },
+  { command: `node server.js ./test/inputPass.txt`, expected: { data: data.expected[1], pass: true } },
+  { command: `cat ./test/inputPass.txt | node server.js`, expected: { data: data.expected[1], pass: true } },
+  { command: `node server.js ./test/inputFail.txt`, expected: { data: data.expected[2], pass: false } },
+  { command: `cat ./test/inputFail.txt | node server.js`, expected: { data: data.expected[2], pass: false } }
 ])
