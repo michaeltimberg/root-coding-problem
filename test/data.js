7210c79f807977a062991a4b965283d26ace4376
Mike Timberg <mike.timberg@technicity.io>
2019-03-29 06:58:44 -0400

    Finished tests for incorrect `"Trip"` commands.

--- test/data.js
+++ test/data.js
@@ -5,6 +5,14 @@ const expected = [
   `Error: No \`"Driver"\` name given!\n` +
   `Error: Bad format for \`"Driver"\` command.\n` +
   `Error: \`"Driver"\` already registered!\n` +
+  `Error: Bad format for \`"Trip"\` command.\n` +
+  `Error: Bad format for \`"Trip"\` command.\n` +
+  `Error: Bad time format for \`"Trip"\` command.\n` +
+  `Error: Bad time format for \`"Trip"\` command.\n` +
+  `Error: Bad time format for \`"Trip"\` command.\n` +
+  `Error: Bad time format for \`"Trip"\` command.\n` +
+  `Error: Bad miles format for \`"Trip"\` command.\n` +
+  `Error: Driver \`"Alex"\` not registered.\n` +
   `Bob: 0 miles\n\n`
 ]
 
